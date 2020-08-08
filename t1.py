2c8cb3e6c7d01bf886c96f51b80ef56cff181482
lekonjak <ricardodogc@gmail.com>
2020-08-08 11:46:46 -0300

    (-) removing redundant information about how to execute it, already present on README.md file.

--- t1.py
+++ t1.py
@@ -1,11 +1,6 @@
 # Ricardo Alves de Araujo - 9364890
 # Tiago Esperança Triques - 9037713
 
-# To test it:
-# python3 -m venv project_dir
-# pip install glfw numpy pyopengl
-# python3 t1.py
-
 import glfw
 from OpenGL.GL import *
 import OpenGL.GL.shaders
